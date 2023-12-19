659018db8bf69990989e6b140f3851f6a252485c
celestin joy <celestinjoy1999@gmail.com>
2023-12-19 23:25:02 +0530

    second commit

--- TodoApplication/src/main/java/com/celestin/TodoApplication/Todo/ToDo.java
+++ TodoApplication/src/main/java/com/celestin/TodoApplication/Todo/ToDo.java
@@ -1,10 +1,16 @@
 package com.celestin.TodoApplication.Todo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
-
+@Entity
 public class ToDo {
+
+    @Id
+    @GeneratedValue
     private int id;
     private String userName;
     @Size(min=10,message = "Enter atleast ten characters")
