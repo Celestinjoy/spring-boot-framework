96571a0da8c4a5daf40aed2dd96cf16c1ee9ff39
celestin joy <celestinjoy1999@gmail.com>
2023-12-29 15:01:41 +0530

    new restapi

--- restful-web-services/src/main/java/com/celestin/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/celestin/restfulwebservices/user/User.java
@@ -1,16 +1,33 @@
 package com.celestin.restfulwebservices.user;
 
+import com.celestin.restfulwebservices.post.Post;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
+import java.util.List;
 
+@Entity(name ="user_details")
 public class User {
+
+    protected User(){
+
+    }
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min=2, message = "More than 2 characters required")
     private String name;
     @Past(message = "Past date is required")
     private LocalDate birthDate;
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Post> posts;
 
     public User(Integer id, String name, LocalDate birthDate) {
         this.id = id;
@@ -40,6 +57,14 @@ public class User {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
